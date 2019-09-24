634af551abfc8f35be9fdd7b462fbd3b32a34b56
JCode1986 <joseph.hangarter@yahoo.com>
2019-09-24 11:31:37 -0700

    updated readme

--- src/shopping.js
+++ src/shopping.js
@@ -10,7 +10,7 @@ function Shopping(props) {
 
   function saveFormDataToServer(formData) {
     let json = JSON.stringify(formData);
-    fetch(API, {
+    fetch(API, { 
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
